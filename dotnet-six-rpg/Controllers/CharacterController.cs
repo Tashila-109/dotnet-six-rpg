4ac08d6250149eb16d40f60e6299cc7082fac808
Tashila <tashila.dev@gmail.com>
2022-10-04 11:18:28 +0400

    Added in character controller

---
+++ dotnet-six-rpg/Controllers/CharacterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_six_rpg.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CharacterController : ControllerBase
+{
+    private static Character _knight = new Character();
+
+    [HttpGet]
+    public ActionResult<Character> Get()
+    {
+        return Ok(_knight);
+    }
+}
--- dotnet-six-rpg/Program.cs
+++ dotnet-six-rpg/Program.cs
@@ -1,3 +1,5 @@
+global using dotnet_six_rpg.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
