b7fa9b198c3916021fd13177052787b1f8a06caa
Tashila <tashila.dev@gmail.com>
2022-10-08 14:44:53 +0400

    Added in data transfer objects

--- dotnet-six-rpg/Controllers/CharacterController.cs
+++ dotnet-six-rpg/Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
-﻿using dotnet_six_rpg.Services.CharacterService;
+﻿using dotnet_six_rpg.Dtos.Character;
+using dotnet_six_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_six_rpg.Controllers;
@@ -15,19 +16,19 @@ public class CharacterController : ControllerBase
     }
 
     [HttpGet("GetAll")]
-    public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+    public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
     {
         return Ok(await _characterService.GetAllCharacters());
     }
 
     [HttpGet("{id:int}")]
-    public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
+    public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id)
     {
         return Ok(await _characterService.GetCharacterById(id));
     }
 
     [HttpPost]
-    public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+    public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
     {
         return Ok(await _characterService.AddCharacter(newCharacter));
     }
--- dotnet-six-rpg/Services/CharacterService/CharacterService.cs
+++ dotnet-six-rpg/Services/CharacterService/CharacterService.cs
@@ -1,4 +1,6 @@
-﻿namespace dotnet_six_rpg.Services.CharacterService;
+﻿using dotnet_six_rpg.Dtos.Character;
+
+namespace dotnet_six_rpg.Services.CharacterService;
 
 public class CharacterService : ICharacterService
 {
@@ -8,22 +10,22 @@ public class CharacterService : ICharacterService
         new Character {Id = 1, Name = "John"}
     };
 
-    public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
+    public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
     {
-        return new ServiceResponse<List<Character>> {Data = _characters};
+        return new ServiceResponse<List<GetCharacterDto>> {Data = _characters};
     }
 
-    public async Task<ServiceResponse<Character>> GetCharacterById(int id)
+    public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
     {
-        var serviceResponse = new ServiceResponse<Character>();
+        var serviceResponse = new ServiceResponse<GetCharacterDto>();
         var character = _characters.FirstOrDefault(c => c.Id == id);
         serviceResponse.Data = character;
         return serviceResponse;
     }
 
-    public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
+    public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
     {
-        var serviceResponse = new ServiceResponse<List<Character>>();
+        var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
         _characters.Add(newCharacter);
         serviceResponse.Data = _characters;
         return serviceResponse;
